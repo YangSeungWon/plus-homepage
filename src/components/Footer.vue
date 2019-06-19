00735550102415daccfb4a074a9812f0289f5eba
GeunWoo Kim <posgnu@kodebox.io>
2019-06-19 17:23:49 +0800

    Change the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,38 +1,42 @@
 <template>
   <div>
     <div class="footer fade-component" v-if="$route.name !== 'Home'">
-      <h2 class="divider"><span></span></h2>
+      <h2 class="divider">
+        <span></span>
+      </h2>
       <div class="footer-inner">
         <div class="copyright">
-          &copy; With Vue.js & GSAP by Mirza Hodzic / <a href="https://coidea.website" target="_blank">COIDEA</a>
+          &copy; With Vue.js & GSAP /
+          <a
+            href="https://github.com/GNUp/PLUS-homepage"
+            target="_blank"
+          >repository</a>
         </div>
         <div class="extra">
           <ul>
             <li>
-              <router-link :to="'/author'" exact>
-                author<span>.</span>
+              <router-link :to="'/about'" exact>
+                About
+                <span>.</span>
               </router-link>
             </li>
             <li>
               <router-link :to="'/works'" exact>
-                works<span>.</span>
+                Works
+                <span>.</span>
               </router-link>
             </li>
             <li>
-              <router-link :to="'/contact'" exact>
-                contact<span>.</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link :to="'/imprint'" exact>
-                imprint<span>.</span>
+              <router-link :to="'/members'" exact>
+                Members
+                <span>.</span>
               </router-link>
             </li>
           </ul>
           <ul>
             <li>
-              <a href="mailto:hello@coidea.website" class="green">
-                <strong>hello@coidea</strong>
+              <a href="mailto:posgnu@gmail.com" class="green">
+                <strong>posgnu@gmail.com</strong>
               </a>
             </li>
           </ul>
@@ -43,50 +47,46 @@
 </template>
 
 <script>
-
-  export default {
-    name: 'AppFooter',
-    data () {
-      return {
-        
-      }
-    }
+export default {
+  name: "AppFooter",
+  data() {
+    return {};
   }
-
+};
 </script>
 
 <style lang="scss" scoped>
-  .footer {
+.footer {
+  position: relative;
+  display: block;
+  width: 100%;
+  .footer-inner {
     position: relative;
     display: block;
     width: 100%;
-    .footer-inner {
-      position: relative;
-      display: block;
-      width: 100%;
-      height: auto;
-      padding: 32px 0;
-      overflow: hidden;
-      font-size: 16px;
-      color: #cccccc;
-      text-align: center;
-      ul {
-        padding: 0;
-        margin: 0;
-        list-style-type: none;
-        li {
-          display: inline-block;
-          padding-right: .75rem;
-          a {
-            font-family: 'Cardo', serif;
-            font-size: 14px;
-            font-weight: 300;
-            &.green {
-              color: #a5d6a7 !important;
-            }
+    height: auto;
+    padding: 32px 0;
+    overflow: hidden;
+    font-size: 16px;
+    color: #cccccc;
+    text-align: center;
+    ul {
+      padding: 0;
+      margin: 0;
+      list-style-type: none;
+      li {
+        display: inline-block;
+        padding-right: 0.75rem;
+        a {
+          font-family: "Cardo", serif;
+          font-size: 14px;
+          font-weight: 300;
+          &.green {
+            color: #a5d6a7 !important;
           }
         }
       }
     }
   }
+}
 </style>
